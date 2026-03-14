36c7a170f6d3f946bca79d599688ae34bb042731
lesaportanko82-debug <lesaportanko82@gmail.com>
2026-03-14 15:20:33 +0500

    Update kv_store.tsx

--- kv_store.tsx
+++ kv_store.tsx
@@ -13,8 +13,8 @@ CREATE TABLE kv_store_b80b3260 (
 import { createClient } from "@supabase/supabase-js";
 
 const client = () => createClient(
-  Deno.env.get("SUPABASE_URL"),
-  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"),
+ process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // Set stores a key-value pair in the database.
